5d2b367d8ee432d1b7b37474a404b874820f8444
11002084 <36611325+11002084@users.noreply.github.com>
2020-10-06 10:44:31 -0700

    10/6/2020
    I finished The Wrong Way Cow yayyyyy

--- src/extras/the_wrong_way_cow/TheWrongWayCow.java
+++ src/extras/the_wrong_way_cow/TheWrongWayCow.java
@@ -46,29 +46,76 @@ public class TheWrongWayCow {
     public static int[] findWrongWayCow(final char[][] field) {
         // Fill in the code to return the x,y coordinate position of the
         // head (letter 'c') of the wrong way cow!
-    	int left = 0;
-    	int right = 0;
-    	int up = 0;
-    	int down = 0;
+    	int[][] checkArr = new int[3][3];
+    	int[] resultArr = new int[0];
+    	int arrSpacesLeft = 3;
     	
-        for(int i = 0; i < field.length; i++) {
-        	for(int j = 0; j < field[i].length; j++) {
+        for(int j = 0; j < field.length; j++) {
+        	for(int i = 0; i < field[j].length; i++) {
         		if(field[j][i] == 'c') {
         			if(i >= 2 && field[j][i-1] == 'o') {
-        				left++;
+        				System.out.println(0);
+        				resultArr = changeArr(i, j, 0, checkArr, arrSpacesLeft);
+        				if(resultArr.length != 0) {
+        					System.out.println(resultArr[0] + " " + resultArr[1]);
+        					return resultArr;
+        				}
         			}
-        			else if(i <= field[i].length - 3 && field[j][i+1] == 'o') {
-        				right++;
+        			else if(i <= field[j].length - 3 && field[j][i+1] == 'o') {
+        				System.out.println(1);
+        				resultArr = changeArr(i, j, 1, checkArr, arrSpacesLeft);
+        				if(resultArr.length != 0) {
+        					System.out.println(resultArr[0] + " " + resultArr[1]);
+        					return resultArr;
+        				}
         			}
         			else if(j >= 2 && field[j-1][i] == 'o') {
-        				up++;
+        				System.out.println(2);
+        				resultArr = changeArr(i, j, 2, checkArr, arrSpacesLeft);
+        				if(resultArr.length != 0) {
+        					System.out.println(resultArr[0] + " " + resultArr[1]);
+        					return resultArr;
+        				}
         			}
-        			else if(j <= field.length && field[j+1][i] == 'o') {
-        				down++;
+        			else if(j <= field.length - 3 && field[j+1][i] == 'o') {
+        				System.out.println(3);
+        				resultArr = changeArr(i, j, 3, checkArr, arrSpacesLeft);
+        				if(resultArr.length != 0) {
+        					System.out.println(resultArr[0] + " " + resultArr[1]);
+        					return resultArr;
+        				}
+        			}
+        			
+        			if(arrSpacesLeft > 0) {
+        				arrSpacesLeft--;
         			}
         		}
         	}
         }
         return null;
+    }
+    
+    static int[] changeArr(int dimensionOne, int dimensionTwo, int direction, int[][] checkArr, int spacesLeft) {
+    	if(spacesLeft > 1) {
+    		checkArr[spacesLeft - 1][0] = dimensionOne;
+    		checkArr[spacesLeft - 1][1] = dimensionTwo;
+    		checkArr[spacesLeft - 1][2] = direction;
+    		return new int[0];
+    	} else {
+    		checkArr[0][0] = dimensionOne;
+    		checkArr[0][1] = dimensionTwo;
+    		checkArr[0][2] = direction;
+    		if(checkArr[0][2] == checkArr[1][2] && checkArr[0][2] == checkArr[2][2]) {
+    			return new int[0];
+    		} else if(checkArr[0][2] != checkArr[1][2] && checkArr[0][2] != checkArr[2][2]) {
+    			return new int[] {checkArr[0][0], checkArr[0][1]};
+    		} else if(checkArr[1][2] != checkArr[0][2] && checkArr[1][2] != checkArr[2][2]) {
+    			return new int[] {checkArr[1][0], checkArr[1][1]};
+    		} else if(checkArr[2][2] != checkArr[0][2] && checkArr[2][2] != checkArr[1][2]){
+    			return new int[] {checkArr[2][0], checkArr[2][1]};
+    		} else {
+    			return new int[0];
+    		}
+    	}
     }
 }
